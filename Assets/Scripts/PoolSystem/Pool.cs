6b8d6c5e4928b52e6c697e9ebf9f3b7aa9f42cf5
1223002814 <1223002814@qq.com>
2021-06-04 01:07:31 +0800

    对象池02

--- Assets/Scripts/PoolSystem/Pool.cs
+++ Assets/Scripts/PoolSystem/Pool.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Pool
 {
     public GameObject Prefab { get => prefab; }
-    [SerializeField] GameObject prefab;
+    [SerializeField] private GameObject prefab;
     [SerializeField] private int poolSize = 1;
     private Queue<GameObject> queue;
     private Transform parent;
@@ -20,6 +20,10 @@ public class Pool
             queue.Enqueue(CreatePreparedObject());
         }
     }
+    /// <summary>
+    /// 创建空闲的游戏对象。
+    /// </summary>
+    /// <returns></returns>
     private GameObject CreatePreparedObject()
     {
         var preparedObject = GameObject.Instantiate(prefab, parent);
@@ -27,9 +31,11 @@ public class Pool
         return preparedObject;
     }
     /// <summary>
-    /// 从对象池中取出对象
+    /// 从对象池中取出不在使用的游戏对象。
     /// </summary>
-    /// <returns></returns>
+    /// <returns>
+    /// <para>对象池中空闲的游戏对象。</para>
+    /// </returns>
     private GameObject AvailableObject()
     {
         GameObject availableObject = null;
@@ -44,12 +50,25 @@ public class Pool
         queue.Enqueue(availableObject);
         return availableObject;
     }
+    /// <summary>
+    /// 取得已激活的可用游戏对象。
+    /// </summary>
+    /// <returns>
+    /// <para>返回已激活的可用游戏对象。</para>
+    /// </returns>
     public GameObject PreparedObject()
     {
         GameObject preparedObject = AvailableObject();
         preparedObject.SetActive(true);
         return preparedObject;
     }
+    /// <summary>
+    /// 取得已激活的可用游戏对象，并设置其<paramref name="position"></paramref>。
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns>
+    /// <para>返回已激活的可用游戏对象。</para>
+    /// </returns>
     public GameObject PreparedObject(Vector3 position)
     {
         GameObject preparedObject = AvailableObject();
@@ -57,6 +76,13 @@ public class Pool
         preparedObject.transform.position = position;
         return preparedObject;
     }
+    /// <summary>
+    /// 取得已激活的可用游戏对象，并设置其<paramref name="position"></paramref>，<paramref name="rotation"></paramref>。
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns>
+    /// <para>返回已激活的可用游戏对象。</para>
+    /// </returns>
     public GameObject PreparedObject(Vector3 position, Quaternion rotation)
     {
         GameObject preparedObject = AvailableObject();
@@ -65,6 +91,13 @@ public class Pool
         preparedObject.transform.rotation = rotation;
         return preparedObject;
     }
+    /// <summary>
+    /// 取得已激活的可用游戏对象，并设置其<paramref name="position"></paramref>，<paramref name="rotation"></paramref>，<paramref name="localScale"></paramref>。
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns>
+    /// <para>返回已激活的可用游戏对象。</para>
+    /// </returns>
     public GameObject PreparedObject(Vector3 position, Quaternion rotation, Vector3 localScale)
     {
         GameObject preparedObject = AvailableObject();
--- Assets/Scripts/PoolSystem/PoolManager.cs
+++ Assets/Scripts/PoolSystem/PoolManager.cs
@@ -1,19 +1,135 @@
+using System.Drawing;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
-    [SerializeField] Pool[] playerProjectilePools;
+    [SerializeField] Pool[] objectPools;
+    private static Dictionary<GameObject, Pool> poolDictionary;
 
-    private void Start() {
-        Init(playerProjectilePools);
+    private void Start()
+    {
+        poolDictionary = new Dictionary<GameObject, Pool>();
+        Init(objectPools);
     }
+    /// <summary>
+    /// <para>根据传入的<paramref name="pools"></paramref>对象池数组，初始化对象池。</para>
+    /// </summary>
+    /// <param name="pools"></param>
     private void Init(Pool[] pools)
     {
-        foreach(var pool in pools)
+        foreach (var pool in pools)
         {
-            Transform poolParent =  new GameObject("Pool: " + pool.Prefab.name).transform;
+#if UNITY_EDITOR
+            if (poolDictionary.ContainsKey(pool.Prefab))
+            {
+                Debug.LogError("Same Prefab in multiple pools!! Pool: " + pool.Prefab.name);
+                continue;
+            }
+#endif
+            poolDictionary.Add(pool.Prefab, pool);
+            Transform poolParent = new GameObject("Pool: " + pool.Prefab.name).transform;
             poolParent.parent = transform;
             pool.Init(poolParent);
         }
+    }
+
+    /// <summary>
+    /// <para>Return a specified<paramref name="prefab"></paramref>gameObject in the pool.</para>
+    /// <para>根据传入的<paramref name="prefab"></paramref>参数，返回对象池中准备好的游戏对象。</para>
+    /// </summary>
+    /// <param name="prefab">
+    /// <para>Specified gameObject prefab.</para>
+    /// <para>指定的游戏对象预制体。</para>
+    /// </param>
+    /// <returns>
+    /// <para>Prepared gameObject in the pool.</para>
+    /// <para>对象池中预备好的游戏对象。</para>
+    /// </returns>
+    public static GameObject Release(GameObject prefab)
+    {
+#if UNITY_EDITOR
+        if (!poolDictionary.ContainsKey(prefab))
+        {
+            Debug.LogError("Pool Manager could not find prefab: " + prefab.name);
+            return null;
+        }
+#endif
+        return poolDictionary[prefab].PreparedObject();
+    }
+    /// <summary>
+    /// <para>Return a specified<paramref name="prefab"></paramref>gameObject in the pool.</para>
+    /// <para>根据传入的<paramref name="prefab"></paramref>参数，返回对象池中准备好的游戏对象，并设置其<paramref name="position"></paramref>。</para>
+    /// </summary>
+    /// <param name="prefab">
+    /// <para>Specified gameObject prefab.</para>
+    /// <para>指定的游戏对象预制体。</para>
+    /// </param>
+    /// <returns>
+    /// <para>Prepared gameObject in the pool.</para>
+    /// <para>对象池中预备好的游戏对象。</para>
+    /// </returns>
+    public static GameObject Release(GameObject prefab, Vector3 position)
+    {
+#if UNITY_EDITOR
+        if (!poolDictionary.ContainsKey(prefab))
+        {
+            Debug.LogError("Pool Manager could not find prefab: " + prefab.name);
+            return null;
+        }
+#endif
+        return poolDictionary[prefab].PreparedObject(position);
+    }
+    /// <summary>
+    /// <para>Return a specified<paramref name="prefab"></paramref>gameObject in the pool.</para>
+    /// <para>根据传入的<paramref name="prefab"></paramref>参数，返回对象池中准备好的游戏对象，并设置其
+    /// <paramref name="position"></paramref>,
+    /// <paramref name="rotation"></paramref>。</para>
+    /// </summary>
+    /// <param name="prefab">
+    /// <para>Specified gameObject prefab.</para>
+    /// <para>指定的游戏对象预制体。</para>
+    /// </param>
+    /// <returns>
+    /// <para>Prepared gameObject in the pool.</para>
+    /// <para>对象池中预备好的游戏对象。</para>
+    /// </returns>
+    public static GameObject Release(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+#if UNITY_EDITOR
+        if (!poolDictionary.ContainsKey(prefab))
+        {
+            Debug.LogError("Pool Manager could not find prefab: " + prefab.name);
+            return null;
+        }
+#endif
+        return poolDictionary[prefab].PreparedObject(position, rotation);
+    }
+    /// <summary>
+    /// <para>Return a specified<paramref name="prefab"></paramref>gameObject in the pool.</para>
+    /// <para>根据传入的<paramref name="prefab"></paramref>参数，返回对象池中准备好的游戏对象，并设置其
+    /// <paramref name="position"></paramref>,
+    /// <paramref name="rotation"></paramref>，
+    /// <paramref name="localScale"></paramref>。</para>
+    /// </summary>
+    /// <param name="prefab">
+    /// <para>Specified gameObject prefab.</para>
+    /// <para>指定的游戏对象预制体。</para>
+    /// </param>
+    /// <returns>
+    /// <para>Prepared gameObject in the pool.</para>
+    /// <para>对象池中预备好的游戏对象。</para>
+    /// </returns>
+    public static GameObject Release(GameObject prefab, Vector3 position, Quaternion rotation, Vector3 localScale)
+    {
+#if UNITY_EDITOR
+        if (!poolDictionary.ContainsKey(prefab))
+        {
+            Debug.LogError("Pool Manager could not find prefab: " + prefab.name);
+            return null;
+        }
+#endif
+        return poolDictionary[prefab].PreparedObject(position, rotation, localScale);
     }
 }
